6714b86c6f089ceafc676f1e16bf468187787378
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-05-27 01:21:14 -0600

    addeda mobile version for mobile users

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const webpack = require('webpack');
 
@@ -46,6 +47,21 @@ module.exports = {
             }),
             new BundleAnalyzerPlug({
                 analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+            }),
+            new WebpackPwaManifest({
+                name: 'Food Event',
+                short_name: 'Foodies',
+                description: 'An app that allows you to view upcoming food events.',
+                start_url: '../index.html',
+                background_color: '#01579b',
+                theme_color: '#ffffff',
+                fingerprints: false,
+                inject: false,
+                icons: [{
+                    src: path.resolve('assets/img/icons/icon-512x512.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons')
+                }]
             })
         ],
         mode: 'development'
